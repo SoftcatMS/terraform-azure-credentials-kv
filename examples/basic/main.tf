826c1a375070ff8febca10a2d721a40781eb38a8
Nielsen Pierce <nielsenpi@softcat.com>
2023-03-27 08:38:20 +0100

    Update Examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,8 +1,12 @@
 module "credentials" {
   source = "github.com/SoftcatMS/terraform-azure-credentials-kv"
 
-  key_vault_name      = "My_Exiting_KeyVault"
-  resource_group_name = "My_Exiting_KeyVault_ResourceGroup"
+  key_vault_name                 = "My_Exiting_KeyVault"
+  resource_group_name            = "My_Exiting_KeyVault_ResourceGroup"
+  create_bastion_softcat_ssh_key = true
+  bastion_softcat_ssh_key_name   = "Softcat-Bastion"
+
+
   passwords = [
     { name = "web-1" },
     { name = "app-1" },
